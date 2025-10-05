ce788cb6a9435354b1d3745d0fcc346c258dfb28
Jose A Ortiz <joseangelortiz15@gmail.com>
2025-10-04 23:09:11 -0600

    created build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: "export",
+    // Add other configurations as needed, e.g., base path for GitHub Pages
+    // basePath: '/your-repo-name', 
+};
 export default nextConfig;
